a4d447836212d5fbd871fe3578a20bb0d7ab6356
premkumar29820 <premkumar2982003@gmail.com>
2026-09-16 14:31:03 +0530

    updated

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = ({ onLogin }) => {
     setstatus(true); // start login
 
     try {
-      const response = await axios.post(`${import.meta.env.REACT_APP_API_URL}/login`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
         email,
         password
       });

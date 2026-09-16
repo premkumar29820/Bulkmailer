76a9b46e2bd3605f68852f3232a0a2db3129f85b
premkumar29820 <premkumar2982003@gmail.com>
2026-09-16 16:00:33 +0530

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -169,8 +169,4 @@ app.get("/api/emails", authMiddleware, async (req, res) => {   //history fetch a
 });
 
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
